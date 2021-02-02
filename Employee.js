2d64e5c97431ee8d1838818ab7616a2e49c4f071
binayaluitel2 <binayababuluitel@gmail.com>
2021-02-01 23:02:18 -0500

    Initial commit with all the four Classes

---
+++ Employee.js
@@ -0,0 +1,22 @@
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return "Employee";
+  }
+}
+const employee = new Employee('binaya Employee', 1, 'employee@test.com');
+console.log(employee);
+module.exports = Employee;
---
+++ Manager.js
@@ -0,0 +1,18 @@
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+  getRole() {
+    return "Manager";
+  }
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+}
+
+const manager = new Manager('binaya', 1, 'test@test.com', 1011);
+console.log(manager);
+module.exports = Manager;
